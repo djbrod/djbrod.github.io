8c551b8a747dfa2a48f07b298c9a49d29b5d2122
Caleb A. BG <flameoff@yahoo.com>
2019-05-22 12:10:44 -0400

    Added two simply programs

---
+++ files/EditJAVA.java
@@ -0,0 +1,14 @@
+public class EditJAVA {
+    
+    public static void main(String[] args) {
+
+        // I'm a comment! two '//' denote a single line comment
+        // Please put your name in the two double quotes below!
+        String yourName = "";
+
+        // Please change the minus to a plus.
+        // Then compile and run to see your message!
+        System.out.println("Welcome to Java, " - yourName);
+    }
+
+}
