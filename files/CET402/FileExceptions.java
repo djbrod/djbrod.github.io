1230e53d6e7288e657e40f3bf07c50d1a0fb09af
djbrod <47068579+djbrod@users.noreply.github.com>
2019-06-02 11:29:54 -0400

    Create FileExceptions.java

---
+++ files/CET402/FileExceptions.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class FileExceptions {
+    public static void main(String args[]) throws Exception{
+    	 FileInputStream input1 = null;
+         input1 = new FileInputStream("notThere.txt");
+         input1.close();
+    }
+}
+
